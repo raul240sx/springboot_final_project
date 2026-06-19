90ec854f02049a12fe5a48d7a980df9725b23f87
raul.ramirez1401 <raul.ramirez1401@gmail.com>
2026-06-19 17:58:25 +0000

    Creación de archivos base para proyecto

---
+++ src/main/java/com/store/sales_api/SalesApiApplication.java
@@ -0,0 +1,13 @@
+package com.store.sales_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesApiApplication.class, args);
+	}
+
+}
