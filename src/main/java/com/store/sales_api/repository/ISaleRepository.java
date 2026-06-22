870d8b11f376f83e1e6b894b80a8d4b727c8af17
raul.ramirez1401 <raul.ramirez1401@gmail.com>
2026-06-22 18:24:45 +0000

    Creación de Interfaces repository para Client, Sale y Product

---
+++ src/main/java/com/store/sales_api/repository/ISaleRepository.java
@@ -0,0 +1,10 @@
+package com.store.sales_api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.store.sales_api.model.Sale;
+
+
+public interface ISaleRepository extends JpaRepository<Sale, Long>{
+
+}
