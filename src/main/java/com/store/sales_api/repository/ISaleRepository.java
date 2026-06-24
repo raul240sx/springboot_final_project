060582ae9c3851beef0c73739968543f3e64e344
raul.ramirez1401 <raul.ramirez1401@gmail.com>
2026-06-24 23:24:40 +0000

    Eliminacion de metodo de conteo de ventas por dia

--- src/main/java/com/store/sales_api/repository/ISaleRepository.java
+++ src/main/java/com/store/sales_api/repository/ISaleRepository.java
@@ -1,6 +1,5 @@
 package com.store.sales_api.repository;
 
-import java.time.LocalDate;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,6 +10,4 @@ import com.store.sales_api.model.Sale;
 public interface ISaleRepository extends JpaRepository<Sale, Long>{
 
     Optional<Sale> findByCode(String code);
-
-    Long countByDate(LocalDate date);
 }
