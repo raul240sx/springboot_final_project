0a2a34f95d707151f275552071ab414f9d0f28a1
raul.ramirez1401 <raul.ramirez1401@gmail.com>
2026-06-24 23:23:42 +0000

    Se agra la opcion de busqueda de venta por código identificador

--- src/main/java/com/store/sales_api/repository/ISaleRepository.java
+++ src/main/java/com/store/sales_api/repository/ISaleRepository.java
@@ -1,5 +1,8 @@
 package com.store.sales_api.repository;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.store.sales_api.model.Sale;
@@ -7,4 +10,7 @@ import com.store.sales_api.model.Sale;
 
 public interface ISaleRepository extends JpaRepository<Sale, Long>{
 
+    Optional<Sale> findByCode(String code);
+
+    Long countByDate(LocalDate date);
 }
