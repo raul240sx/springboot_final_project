02371d05493439070b97e5fbfa28bd4f934b097d
raul.ramirez1401 <raul.ramirez1401@gmail.com>
2026-06-24 23:03:22 +0000

    Se actualiza el metodo de codificado

--- src/main/java/com/store/sales_api/util/SaleCodeGenerator.java
+++ src/main/java/com/store/sales_api/util/SaleCodeGenerator.java
@@ -8,14 +8,22 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class SaleCodeGenerator {
+    private static final String BASE62_CHARS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
-    public String generateSaleCode(LocalDate date, Long saleCount) {
+    public String generateSaleCode(LocalDate date, Long saleId) {
+        StringBuilder encodedId = new StringBuilder();
+        while (saleId > 0) {
+            int remainder = (int) (saleId % 62);
+            encodedId.append(BASE62_CHARS.charAt(remainder));
+            saleId = saleId / 62;
+        }
+
         
         DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyyMMdd");
 
         String stringDate = date.format(dateFormatter);
-        String saleNumber = String.valueOf(saleCount) + "0".repeat(4 - String.valueOf(saleCount).length());
+        String encodedIdZero = "0".repeat(Math.max(0, 4 - String.valueOf(encodedId).length())) + encodedId.reverse();
 
-        return stringDate + saleNumber;
+        return stringDate + encodedIdZero;
     }
 }
