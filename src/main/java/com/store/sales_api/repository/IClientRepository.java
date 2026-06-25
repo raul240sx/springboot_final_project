34d39073ef1c77eb2c7bacdc1de5168193157354
raul.ramirez1401 <raul.ramirez1401@gmail.com>
2026-06-25 00:03:15 +0000

    Se agrega metodo para buscar clientes a traves de su codigo de cliente

--- src/main/java/com/store/sales_api/repository/IClientRepository.java
+++ src/main/java/com/store/sales_api/repository/IClientRepository.java
@@ -1,5 +1,7 @@
 package com.store.sales_api.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.store.sales_api.model.Client;
@@ -7,4 +9,5 @@ import com.store.sales_api.model.Client;
 
 public interface IClientRepository extends JpaRepository<Client, Long>{
 
+    Optional<Client> findByCode(String code);
 }
