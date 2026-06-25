de2bbcb60fb0fa59d354ceba4b9c24beaef07ee9
raul.ramirez1401 <raul.ramirez1401@gmail.com>
2026-06-25 23:01:36 +0000

    Se agrega query method al repositorio de product para busqueda de productos inferiores a un stock indicado

--- src/main/java/com/store/sales_api/repository/IProductRepository.java
+++ src/main/java/com/store/sales_api/repository/IProductRepository.java
@@ -14,4 +14,6 @@ public interface IProductRepository extends JpaRepository<Product, Long>{
 
     List<Product> findAllByCodeIn(List<String> codes);
 
+    List<Product> findByStockLessThan(Integer stock);
+
 }
