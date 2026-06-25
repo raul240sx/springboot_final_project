164aba32dc3ac903e30e8c41bab09208d1b77e71
raul.ramirez1401 <raul.ramirez1401@gmail.com>
2026-06-25 01:11:39 +0000

    Se agregan busquedas por codigo a repositorio de Product

--- src/main/java/com/store/sales_api/repository/IProductRepository.java
+++ src/main/java/com/store/sales_api/repository/IProductRepository.java
@@ -1,10 +1,17 @@
 package com.store.sales_api.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.store.sales_api.model.Product;
 
 
 public interface IProductRepository extends JpaRepository<Product, Long>{
+
+    Optional<Product> findByCode(String code);
+
+    List<Product> findAllByCodeIn(List<String> codes);
 
 }
