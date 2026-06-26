29f04f55c3051f09c1b2b863b040227f438659d1
raul.ramirez1401 <raul.ramirez1401@gmail.com>
2026-06-26 23:19:17 +0000

    Se añade validación al SaleRequestDTO

--- src/main/java/com/store/sales_api/dto/SaleRequestDTO.java
+++ src/main/java/com/store/sales_api/dto/SaleRequestDTO.java
@@ -2,8 +2,17 @@ package com.store.sales_api.dto;
 
 import java.util.List;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+
 public record SaleRequestDTO(
+
+    @NotBlank(message = "{sale.client.code.null}")
     String clientCode,
+
+    @NotEmpty(message = "{sale.details.empty}")
+    @Valid
     List<DetailRequestDTO> details
 ) {
 
