4a529c2c7237670eb7ce153c84b2a560aee3dee7
raul.ramirez1401 <raul.ramirez1401@gmail.com>
2026-07-08 20:58:50 +0000

    Se migra SaleCodeGenerator a la feature sale

---
+++ src/main/java/com/store/sales_api/sale/SaleCodeGenerator.java
@@ -0,0 +1,31 @@
+package com.store.sales_api.util;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import org.springframework.stereotype.Component;
+
+import com.store.sales_api.common.util.Base62Encoder;
+
+
+@Component
+public class SaleCodeGenerator {
+    private final Base62Encoder encoder;
+
+    public SaleCodeGenerator(Base62Encoder encoder) {
+        this.encoder = encoder;
+    }
+
+
+    public String generateSaleCode(LocalDate date, Long saleId) {
+
+        String encodedId = encoder.generateCode(saleId);
+        
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+
+        String stringDate = date.format(dateFormatter);
+        String encodedIdZero = "0".repeat(Math.max(0, 4 - encodedId.length())) + encodedId;
+
+        return stringDate + encodedIdZero;
+    }
+}
