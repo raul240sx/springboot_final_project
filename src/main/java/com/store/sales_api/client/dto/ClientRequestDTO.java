24427e35b2ed441dd535893ea3005325a04a3d2a
raul.ramirez1401 <raul.ramirez1401@gmail.com>
2026-07-08 20:51:02 +0000

    Se migran DTOs de cliente a una estructura feature-based al sub-paquete dto

---
+++ src/main/java/com/store/sales_api/client/dto/ClientRequestDTO.java
@@ -0,0 +1,14 @@
+package com.store.sales_api.client.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record ClientRequestDTO(
+    @NotBlank(message = "{client.name.null}")
+    String name,
+    @NotBlank(message = "{client.last.name.null}")
+    String lastName,
+    @NotBlank(message = "{client.dni.null}")
+    String dni
+) {
+
+}
---
+++ src/main/java/com/store/sales_api/client/dto/ClientResponseDTO.java
@@ -0,0 +1,10 @@
+package com.store.sales_api.client.dto;
+
+public record ClientResponseDTO(
+    String code,
+    String name,
+    String lastName,
+    String dni
+) {
+
+}
