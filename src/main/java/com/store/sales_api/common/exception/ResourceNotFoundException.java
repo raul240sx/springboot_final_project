c6dddb8e5c3c8779ded886cf8076c917f7ec4ccd
raul.ramirez1401 <raul.ramirez1401@gmail.com>
2026-07-08 21:01:53 +0000

    Se migra el módulo exception y util a una estructura feature-based en el paquete common

---
+++ src/main/java/com/store/sales_api/common/exception/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.store.sales_api.common.exception;
+
+public class ResourceNotFoundException extends RuntimeException{
+    
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+}
---
+++ src/main/java/com/store/sales_api/common/util/Base62Encoder.java
@@ -0,0 +1,23 @@
+package com.store.sales_api.common.util;
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class Base62Encoder {
+    private static final String BASE62_CHARS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    public String generateCode(Long id) {
+
+        if (id == 0) return "0";
+
+        StringBuilder encodedId = new StringBuilder();
+        while (id > 0) {
+            int remainder = (int) (id % 62);
+            encodedId.append(BASE62_CHARS.charAt(remainder));
+            id = id / 62;
+        }
+
+        return encodedId.reverse().toString();
+    }
+}
