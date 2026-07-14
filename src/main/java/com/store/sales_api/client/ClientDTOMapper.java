67377b17c2dea38346f6c239ae584646cbc4b840
raul.ramirez1401 <raul.ramirez1401@gmail.com>
2026-07-14 02:18:36 +0000

    Se agrega la anotacion @Component a ClientDTOMapper

--- src/main/java/com/store/sales_api/client/ClientDTOMapper.java
+++ src/main/java/com/store/sales_api/client/ClientDTOMapper.java
@@ -1,8 +1,13 @@
 package com.store.sales_api.client;
 
+import org.springframework.stereotype.Component;
+
 import com.store.sales_api.client.dto.ClientResponseDTO;
 
+
+@Component
 public class ClientDTOMapper {
+
 
     public ClientResponseDTO clientToDTO(Client client) {
         if (client == null) return null;
