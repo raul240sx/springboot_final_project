9cf44d397d53104bb86e0bfbb41aa62bb42f0f09
raul.ramirez1401 <raul.ramirez1401@gmail.com>
2026-07-14 02:17:32 +0000

    Se crea TokenEncript para encriptación de los refreshToken

---
+++ src/main/java/com/store/sales_api/common/util/TokenEncrypt.java
@@ -0,0 +1,27 @@
+package com.store.sales_api.common.util;
+
+import org.springframework.stereotype.Component;
+
+import com.store.sales_api.common.exception.BusinessRuleException;
+
+
+@Component
+public class TokenEncrypt {
+
+    public String hashToken(String token) {
+    try {
+        java.security.MessageDigest digest = java.security.MessageDigest.getInstance("SHA-256");
+        byte[] hash = digest.digest(token.getBytes(java.nio.charset.StandardCharsets.UTF_8));
+        StringBuilder hexString = new StringBuilder();
+        for (byte b : hash) {
+            String hex = Integer.toHexString(0xff & b);
+            if (hex.length() == 1) hexString.append('0');
+            hexString.append(hex);
+        }
+        return hexString.toString();
+    } catch (java.security.NoSuchAlgorithmException e) {
+        throw new BusinessRuleException("Error crítico al inicializar el algoritmo criptográfico");
+    }
+}
+
+}
