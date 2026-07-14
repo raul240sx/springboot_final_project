83486882f6abdb1676d120632afbbb7ee77d9130
raul.ramirez1401 <raul.ramirez1401@gmail.com>
2026-07-14 19:04:59 +0000

    Se crea AuthController para exponer los endpoints de Login, Logout y refresh

---
+++ src/main/java/com/store/sales_api/security/controller/AuthController.java
@@ -0,0 +1,51 @@
+package com.store.sales_api.security.controller;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.store.sales_api.security.dto.CookiesResponseDTO;
+import com.store.sales_api.security.dto.LoginRequestDTO;
+import com.store.sales_api.security.service.IAuthService;
+
+import jakarta.validation.Valid;
+
+
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    private final IAuthService authService;
+
+
+    public AuthController(IAuthService authService) {
+        this.authService = authService;
+    }
+
+
+    @PostMapping("/login")
+    public ResponseEntity<Void> login(@Valid @RequestBody LoginRequestDTO dto){
+
+        CookiesResponseDTO cookies = authService.login(dto);
+        return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, cookies.accessCookie().toString()).header(HttpHeaders.SET_COOKIE, cookies.refreshCookie().toString()).build();
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<Void> refresh(@CookieValue(name = "REFRESH-TOKEN") String refreshToken) {
+        
+        CookiesResponseDTO cookies = authService.refresh(refreshToken);
+        return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, cookies.accessCookie().toString()).header(HttpHeaders.SET_COOKIE, cookies.refreshCookie().toString()).build();
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout(@CookieValue(name = "REFRESH-TOKEN", required = false) String refreshToken) {
+
+        CookiesResponseDTO cookies = authService.logout(refreshToken);
+        return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, cookies.accessCookie().toString()).header(HttpHeaders.SET_COOKIE, cookies.refreshCookie().toString()).build();
+    }
+
+}
