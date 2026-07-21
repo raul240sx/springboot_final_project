bee01727bd76779790251e4b5060dea8aa3cb2f7
raul.ramirez1401 <raul.ramirez1401@gmail.com>
2026-07-21 17:51:09 +0000

    Se agrega el módulo ms-registry-server para registrar los microservicios con Eureka

---
+++ ms-registry-server/src/test/java/rrs/ms_registry_server/MsRegistryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package rrs.ms_registry_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsRegistryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
