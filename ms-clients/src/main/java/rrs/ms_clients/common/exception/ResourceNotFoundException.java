e5eac80092d05ec3f5ac5bafc25a6f2d7df176e4
raul.ramirez1401 <raul.ramirez1401@gmail.com>
2026-07-31 02:03:39 +0000

    Se replica el paquete de excepciones del monolito al servicio ms-clients añadiendo además la excepción  handleNoResourceFound para los endpoints inexistentes

---
+++ ms-clients/src/main/java/rrs/ms_clients/common/exception/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package rrs.ms_clients.common.exception;
+
+public class ResourceNotFoundException extends RuntimeException{
+    
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+}
