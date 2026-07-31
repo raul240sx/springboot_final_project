a5233cfea412235af53e6270c7ac71ea9dbf1196
raul.ramirez1401 <raul.ramirez1401@gmail.com>
2026-07-31 02:08:41 +0000

    Se replica controller de la feature client del monolito al servicio ms-clients

---
+++ ms-clients/src/main/java/rrs/ms_clients/controller/ClientController.java
@@ -0,0 +1,61 @@
+package rrs.ms_clients.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.validation.Valid;
+import rrs.ms_clients.dto.ClientRequestDTO;
+import rrs.ms_clients.dto.ClientResponseDTO;
+import rrs.ms_clients.service.IClientService;
+
+
+@Controller
+@RequestMapping("/api/clients")
+public class ClientController {
+    private final IClientService clientService;
+
+    public ClientController(IClientService clientService) {
+        this.clientService = clientService;
+    }
+
+
+    @GetMapping
+    public ResponseEntity<List<ClientResponseDTO>> getAllClients() {
+        return ResponseEntity.status(HttpStatus.OK).body(clientService.getClients());
+    }
+
+    @GetMapping("/{clientCode}")
+    public ResponseEntity<ClientResponseDTO> getclient(@PathVariable String clientCode) {
+        return ResponseEntity.status(HttpStatus.OK).body(clientService.getClient(clientCode));
+    }
+
+    @PostMapping
+    public ResponseEntity<ClientResponseDTO> createClient(@Valid @RequestBody ClientRequestDTO clientDTO) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(clientService.createClient(clientDTO));
+    }
+
+    @PutMapping("/{clientCode}")
+    public ResponseEntity<ClientResponseDTO> updateClient(
+        @PathVariable String clientCode,
+        @Valid @RequestBody ClientRequestDTO clientDTO
+    ) {
+        return ResponseEntity.status(HttpStatus.OK).body(clientService.updateClient(clientCode, clientDTO));
+    }
+
+    @DeleteMapping("/{clientCode}")
+    public ResponseEntity<Void> deleteClient(@PathVariable String clientCode) {
+        clientService.deleteClient(clientCode);
+        return ResponseEntity.noContent().build();
+    }
+
+}
