f2bb8ddc158ed461bcb09ddf3ab92cf7fec08ff4
raul.ramirez1401 <raul.ramirez1401@gmail.com>
2026-07-31 14:24:26 +0000

    Se crean archivos genericos de pproyecto para el servicio ms-sales

---
+++ ms-sales/src/test/java/rrs/ms_sales/MsSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package rrs.ms_sales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
