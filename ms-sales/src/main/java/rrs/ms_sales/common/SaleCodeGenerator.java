53ddaeb52cf645c6bd549ed0c8444d3ec6033ef4
raul.ramirez1401 <raul.ramirez1401@gmail.com>
2026-07-31 14:32:50 +0000

    Se replica el archivo SaleCodeGenerator del monolito al servicio ms-sales

---
+++ ms-sales/src/main/java/rrs/ms_sales/common/SaleCodeGenerator.java
@@ -0,0 +1,31 @@
+package rrs.ms_sales.common;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import org.springframework.stereotype.Component;
+
+import rrs.ms_sales.common.util.Base62Encoder;
+
+
+@Component
+public class SaleCodeGenerator {
+    private final Base62Encoder encoder;
+
+    public SaleCodeGenerator(Base62Encoder encoder) {
+        this.encoder = encoder;
+    }
+
+
+    public String generateSaleCode(LocalDate date, Long saleId) {
+
+        String encodedId = encoder.generateCode(saleId);
+        
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+
+        String stringDate = date.format(dateFormatter);
+        String encodedIdZero = "0".repeat(Math.max(0, 4 - encodedId.length())) + encodedId;
+
+        return stringDate + encodedIdZero;
+    }
+}
