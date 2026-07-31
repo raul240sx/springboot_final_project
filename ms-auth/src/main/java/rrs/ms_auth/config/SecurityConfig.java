2a63894f00688c7bd65bc8b5c3e68ac7ecdd8759
raul.ramirez1401 <raul.ramirez1401@gmail.com>
2026-07-31 01:22:47 +0000

    Se replica el SecurityConfig de la feature security del monolito al servicio ms-auth quitandole la responsabilidad de CORS y de apretura de Cookies

---
+++ ms-auth/src/main/java/rrs/ms_auth/config/SecurityConfig.java
@@ -0,0 +1,117 @@
+package rrs.ms_auth.config;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+
+import rrs.ms_auth.common.exception.CustomAuthenticationEntryPoint;
+
+
+
+
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+    private final CustomAuthenticationEntryPoint authenticationEntryPoint;
+
+    @Value("${spring.security.oauth2.resourceserver.jwt.public-key-location}")
+    private RSAPublicKey publicKey;
+
+    @Value("${spring.security.oauth2.resourceserver.jwt.private-key-location}")
+    private RSAPrivateKey privateKey;
+
+    @Value("${app.security.public-urls}")
+    private String[] publicUrls;
+
+
+
+    
+    public SecurityConfig(CustomAuthenticationEntryPoint authenticationEntryPoint) {
+        this.authenticationEntryPoint = authenticationEntryPoint;
+    }
+
+
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .sessionManagement((sessionManagement) -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .csrf((csrf) -> csrf.disable())
+            .authorizeHttpRequests((authorize) -> authorize
+                .requestMatchers(publicUrls).permitAll()
+                .anyRequest().authenticated()
+            )
+            .oauth2ResourceServer((oauth2) -> oauth2
+                .jwt((jwt) -> jwt
+                    .decoder(jwtDecoder())
+                    .jwtAuthenticationConverter(jwtAuthenticationConverter())
+                )
+                .authenticationEntryPoint(authenticationEntryPoint)
+            );
+        return http.build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withPublicKey(this.publicKey).build();
+    }
+
+    @Bean
+    public JwtEncoder jwtEncoder() {
+        RSAKey key = new RSAKey.Builder(this.publicKey).privateKey(this.privateKey).build();
+        JWKSet keySet = new JWKSet(key);
+        JWKSource<SecurityContext> jwkSource = new ImmutableJWKSet<>(keySet);
+        return new NimbusJwtEncoder(jwkSource);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        grantedAuthoritiesConverter.setAuthoritiesClaimName("roles");
+        grantedAuthoritiesConverter.setAuthorityPrefix("");
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
+        return jwtAuthenticationConverter;
+    }
+
+
+}
