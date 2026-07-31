5e4e9679fbae2304894a1783b17f267f19c9aada
raul.ramirez1401 <raul.ramirez1401@gmail.com>
2026-07-31 14:51:26 +0000

    Se replica la interfaz repositorio de la feature sale del monolito al servicio ms-sales

---
+++ ms-sales/src/main/java/rrs/ms_sales/repository/ISaleRepository.java
@@ -0,0 +1,25 @@
+package rrs.ms_sales.repository;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import rrs.ms_sales.model.Sale;
+
+
+public interface ISaleRepository extends JpaRepository<Sale, Long>{
+
+    Optional<Sale> findByCode(String code);
+
+    @Query("SELECT COUNT(s) FROM Sale s WHERE s.date = :date")
+    Integer getDaySalesCount(@Param("date") LocalDate date);
+
+    @Query("SELECT SUM(s.totalAmount) FROM Sale s WHERE s.date = :date")
+    BigDecimal getDayTotalAmount(@Param("date") LocalDate date);
+
+    Optional<Sale> findTopByOrderByTotalAmountDesc();
+}
